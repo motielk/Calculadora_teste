a4de25b2c0f49ffb31212284047579d21db08f6c
Murilo H. Gonçalves <49123696+motielk@users.noreply.github.com>
2024-03-25 22:34:42 -0300

    Atualizando A Calculadora

---
+++ Calculadora_teste-main/Exercicio1/src/main/java/Calculadora/CalculadoraMulti.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Calculadora;
+
+/**
+ *
+ * @author Murilo
+ */
+public class CalculadoraMulti {
+    //Classe de multiplicação de dois ints
+    public int multi (int a, int b){
+        return a * b;
+    }
+}
