07c22e5316e5a29d2320a59e014958ad20ef6237
Murilo H. Gonçalves <49123696+motielk@users.noreply.github.com>
2024-03-25 22:36:01 -0300

    Calculadora Com JunitTest

---
+++ Calculadora_teste-main1/Exercicio1/src/main/java/Calculadora/CalculadoraDIv.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Calculadora;
+
+/**
+ *
+ * @author Murilo
+ */
+public class CalculadoraDIv {
+    //Classe de divisão de dois ints
+    public int divisao (int a, int b){
+        return a / b;
+    }
+
+}
